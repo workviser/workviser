5e3a33088315b6f88a8c88d0e6646240f549586a
workviser <workviseraws@gmail.com>
2025-07-02 20:36:04 +0530

    Update TaskSchema.py

--- app/APIValidation/TaskSchema.py
+++ app/APIValidation/TaskSchema.py
@@ -73,7 +73,8 @@ class TaskCreate(TaskBase):
         example="proj_12345",
         description="ID of the project this task belongs to",
         pattern=r'^[a-zA-Z0-9_]+$'
-    )
+    ),
+    domain:list[str]
 
 
     class Config:
